a8340ccdbc96a7c763ef7ffadd07a74e91008bf5
Jan Moravec <moravecjan@email.cz>
2019-12-08 19:42:53 +0100

    Added example of reading connected cameras and its properties.

---
+++ 01-camera-detection/main.cpp
@@ -0,0 +1,38 @@
+#include <QDebug>
+#include <QCamera>
+#include <QCameraInfo>
+
+int main()
+{
+    QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
+
+    qDebug() << "Detected cameras:" << cameras.size();
+
+    for (const QCameraInfo &info: cameras) {
+        qDebug() << "Opening camera:" << info.deviceName();
+        qDebug() << "\t- Description:" << info.description();
+        qDebug() << "\t- Position:" << info.position();
+
+        QCamera camera(info);
+        QList<QSize> resolutions = camera.supportedViewfinderResolutions();
+        QList<QVideoFrame::PixelFormat> pixelformats = camera.supportedViewfinderPixelFormats();
+        QList<QCamera::FrameRateRange> framerates = camera.supportedViewfinderFrameRateRanges();
+
+        qDebug() << "\t- Resolutions:";
+        for (const QSize &resolution : resolutions) {
+            qDebug() << "\t\t-" << resolution.width() << "x" << resolution.height();
+        }
+
+        qDebug() << "\t- Pixel Formats:";
+        for (const QVideoFrame::PixelFormat &format : pixelformats) {
+            qDebug() << "\t\t-" << format;
+        }
+
+        qDebug() << "\t- Frame Rates:";
+        for (const QCamera::FrameRateRange &rate : framerates) {
+            qDebug() << "\t\t-" << rate.minimumFrameRate << "to" << rate.maximumFrameRate;
+        }
+    }
+
+    return 0;
+}
